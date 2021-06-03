fec78b65b0c00e8a3a19aaed4b545a1170adb978
willcod <william.wow@icloud.com>
2021-06-03 14:09:05 +0800

    Add project files.

---
+++ WPF_Localization_on-the-fly/CultureResources.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WPF_Localization_on_the_fly
+{
+    public class CultureResources
+    {
+        private static ObjectDataProvider _provider;
+
+        public static ObjectDataProvider ResourceProvider
+        {
+            get {
+                return _provider ?? (_provider = (ObjectDataProvider) Application.Current.FindResource("CultureRes"));
+            }
+        }
+
+        public static void ChangeCulture(CultureInfo culture)
+        {
+            Properties.Resources.Culture = culture;
+            ResourceProvider.Refresh();
+        }
+
+        public Properties.Resources GetResourceInstance()
+        {
+            return new Properties.Resources();
+        }
+    }
+}
---
+++ WPF_Localization_on-the-fly/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Localization_on_the_fly
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static bool _flag = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SwitchButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!_flag)
+            {
+                var culture = new CultureInfo("zh-CN");
+                CultureResources.ChangeCulture(culture);
+            }
+            else
+            {
+                var culture = new CultureInfo("en-US");
+                CultureResources.ChangeCulture(culture);
+            }
+
+            _flag = !_flag;
+        }
+    }
+}
